016ad394c81e3c0a42f71b426748f8a3e311b682
Alberto Calvo <intemperie@gmail.com>
2013-06-01 16:01:15 +0200

    small tweaks and comments

--- js/main.js
+++ js/main.js
@@ -58,12 +58,9 @@ $(window).on("load scroll",function(e){
 	});
 });
 
-
+// !WIP
 var toggleSlide = function(){
 	$(".brands li.active").removeClass();
-	$('.brands li:random').addClass('active');
-
-//  $(".brands li.active").removeClass()
 //   .next().add(".brands li:first").last().addClass("active");
 }
 setInterval(toggleSlide, 2000);
